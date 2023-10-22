62af1f7902a040d51cf3fd327905e45b56e90d0d
isaacnwt <isaacnewton02@hotmail.com>
2023-10-22 15:25:07 -0300

    rerfactor: style and validation improvements

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -14,7 +14,7 @@ export class RegisterComponent implements OnInit {
   ngOnInit() {
     this.form = this.formBuilder.group({
       firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
+      lastName: [''],
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(8)]]
     })
