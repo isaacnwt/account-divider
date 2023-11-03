dfec758455aadd8404eb6ae4b5399473cf7c4696
isaacnwt <isaacnewton02@hotmail.com>
2023-11-03 00:00:58 -0300

    chore: replacing old project name references

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'account-divider'`, () => {
+  it(`should have as title 'bill-splitter'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('account-divider');
+    expect(app.title).toEqual('bill-splitter');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('account-divider app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('bill-splitter app is running!');
   });
 });
